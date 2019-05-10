f0ca72df1b80f2561ee576ce0b46236bbd873f1f
Paul Alarcon <pag66.06@gmail.com>
2019-05-09 23:53:40 -0500

    seguridades

--- src/main/java/com/krugercorp/ec/micro1/Micro1Application.java
+++ src/main/java/com/krugercorp/ec/micro1/Micro1Application.java
@@ -3,7 +3,11 @@ package com.krugercorp.ec.micro1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
+@EnableAuthorizationServer
 @SpringBootApplication
 public class Micro1Application {
 
